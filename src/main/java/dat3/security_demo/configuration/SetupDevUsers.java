633217a59cba32ea437ee49634331271eca10783
Jaes98 <marcus.jaes@gmail.com>
2024-03-07 08:33:04 +0100

    weird ting

--- src/main/java/dat3/security_demo/configuration/SetupDevUsers.java
+++ src/main/java/dat3/security_demo/configuration/SetupDevUsers.java
@@ -1,9 +1,12 @@
-package dat3.rename_me.configuration;
+package dat3.security_demo.configuration;
 
 import dat3.security.entity.Role;
 import dat3.security.entity.UserWithRoles;
 import dat3.security.repository.RoleRepository;
 import dat3.security.repository.UserWithRolesRepository;
+import dat3.security_demo.entity.SpecialUser;
+import dat3.security_demo.repository.SpecialUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +21,9 @@ public class SetupDevUsers implements ApplicationRunner {
     RoleRepository roleRepository;
     PasswordEncoder pwEncoder;
     String passwordUsedByAll;
+    @Autowired
+    SpecialUserRepository specialUserRepository;
+
 
     public SetupDevUsers(UserWithRolesRepository userWithRolesRepository,RoleRepository roleRepository,PasswordEncoder passwordEncoder) {
         this.userWithRolesRepository = userWithRolesRepository;
@@ -66,5 +72,10 @@ public class SetupDevUsers implements ApplicationRunner {
         userWithRolesRepository.save(user2);
         userWithRolesRepository.save(user3);
         userWithRolesRepository.save(user4);
+        SpecialUser specialUser =
+                new SpecialUser("specialUser",pwEncoder.encode(passwordUsedByAll),"s@a.dk","Anders","Hansen","Lyngby vej 23","2800","Lyngby");
+        specialUser.addRole(roleUser);
+        specialUserRepository.save(specialUser);
+
     }
 }
